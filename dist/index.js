16ed130c7ebb0e8895de2adfac3efcfe3631a109
Narendran <narendran.i@xseededucation.com>
2024-06-08 16:34:30 +0530

    remove title

--- dist/index.js
+++ dist/index.js
@@ -292,9 +292,8 @@ const construct_card_1 = __nccwpck_require__(3377);
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            core.setCommandEcho(true);
             const webhookUrl = core.getInput('webhook-url');
-            const title = core.getInput('title') || 'NA';
+            const title = 'NA';
             const creator = core.getInput('creator-name');
             const assetUrl = core.getInput('asset-url');
             const description = core.getInput('description');
--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ async function run() {
   try {
 
     const webhookUrl = core.getInput('webhook-url');
-    const title = core.getInput('title') || 'NA';
+    const title =  'NA';
     const creator = core.getInput('creator-name');
     const assetUrl = core.getInput('asset-url');
     const description = core.getInput('description');
