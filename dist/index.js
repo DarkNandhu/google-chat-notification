c58a6c76ed87a63248ad931807693390df4710e7
Narendran <narendran.i@xseededucation.com>
2024-06-08 17:27:47 +0530

    update dist

--- dist/index.js
+++ dist/index.js
@@ -292,27 +292,33 @@ const construct_card_1 = __nccwpck_require__(3377);
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const webhookUrl = core.getInput('webhook-url');
-            const title = core.getInput('title');
-            const creator = core.getInput('creator-name');
-            const assetUrl = core.getInput('asset-url');
-            const description = core.getInput('description');
-            const jobStatus = core.getInput('job-status');
-            const commitId = core.getInput('commit-id');
-            const payload = Object.assign({}, new construct_card_1.ConstructCard({
-                'header': {
-                    'title': title || 'NA',
-                    'subtitle': 'NA'
-                },
-                'creator_name': creator || 'NA',
-                'asset_url': assetUrl || 'NA',
-                'body': description || 'NA',
-                'job_status': jobStatus || 'NA',
-                'commit_id': commitId || 'NA'
-            }).get());
+            const webhookUrl = core.getInput("webhook-url");
+            const title = core.getInput("title");
+            const creator = core.getInput("creator-name");
+            const assetUrl = core.getInput("asset-url");
+            const description = core.getInput("description");
+            const jobStatus = core.getInput("job-status");
+            const commitId = core.getInput("commit-id");
+            const payload = {
+                cardsV2: [
+                    {
+                        card: Object.assign({}, new construct_card_1.ConstructCard({
+                            header: {
+                                title: title || "NA",
+                                subtitle: "NA",
+                            },
+                            creator_name: creator || "NA",
+                            asset_url: assetUrl || "NA",
+                            body: description || "NA",
+                            job_status: jobStatus || "NA",
+                            commit_id: commitId || "NA",
+                        }).get()),
+                    },
+                ],
+            };
             console.log(JSON.stringify(payload));
             yield axios_1.default.post(webhookUrl, payload);
-            core.info('Notification sent successfully.');
+            core.info("Notification sent successfully.");
         }
         catch (error) {
             core.setFailed(`Action failed with error: ${error}`);
--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,40 @@
-import * as core from '@actions/core';
+import * as core from "@actions/core";
 import axios from "axios";
 import { ConstructCard } from "./helpers/construct-card";
 
-
 async function run() {
   try {
-    const webhookUrl = core.getInput('webhook-url');
-    const title = core.getInput('title');
-    const creator = core.getInput('creator-name');
-    const assetUrl = core.getInput('asset-url');
-    const description = core.getInput('description');
-    const jobStatus = core.getInput('job-status');
-    const commitId = core.getInput('commit-id');
+    const webhookUrl = core.getInput("webhook-url");
+    const title = core.getInput("title");
+    const creator = core.getInput("creator-name");
+    const assetUrl = core.getInput("asset-url");
+    const description = core.getInput("description");
+    const jobStatus = core.getInput("job-status");
+    const commitId = core.getInput("commit-id");
 
     const payload: Record<string, any> = {
-        ...new ConstructCard(
-          {
-            'header': {
-              'title': title || 'NA',
-              'subtitle': 'NA'
-            },
-            'creator_name': creator || 'NA',
-            'asset_url': assetUrl || 'NA',
-            'body': description || 'NA',
-            'job_status': jobStatus || 'NA',
-            'commit_id': commitId || 'NA'
-          }
-        ).get()
+      cardsV2: [
+        {
+          card: {
+            ...new ConstructCard({
+              header: {
+                title: title || "NA",
+                subtitle: "NA",
+              },
+              creator_name: creator || "NA",
+              asset_url: assetUrl || "NA",
+              body: description || "NA",
+              job_status: jobStatus || "NA",
+              commit_id: commitId || "NA",
+            }).get(),
+          },
+        },
+      ],
     };
 
-    console.log(JSON.stringify(payload))
+    console.log(JSON.stringify(payload));
     await axios.post(webhookUrl, payload);
-    core.info('Notification sent successfully.');
+    core.info("Notification sent successfully.");
   } catch (error: any) {
     core.setFailed(`Action failed with error: ${error}`);
   }
