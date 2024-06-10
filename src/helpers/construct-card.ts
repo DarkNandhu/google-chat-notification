dd282479ef6b7a611b228d34f961029251926a72
Narendran <narendran.i@xseededucation.com>
2024-06-10 20:19:38 +0530

    update notification code

--- src/helpers/construct-card.ts
+++ src/helpers/construct-card.ts
@@ -14,9 +14,8 @@ import {
   StatusMessageKey,
 } from "./success-indication";
 import * as core from "@actions/core";
+import { BodyConstructor } from "./body-helpers/BodyConstructor";
 
-const branchIconUrl =
-  "https://raw.githubusercontent.com/xseededucation/action_assets/master/git-branch-128.png";
 
 export class ConstructCard {
   inputJson: Record<string, any>;
@@ -35,112 +34,7 @@ export class ConstructCard {
   }
 
   getBodySections(): Record<string, any> {
-    let sections: Sections = new Sections([]);
-    if (this.inputJson.job_status) {
-      const jobStatus: string = this.inputJson.job_status;
-      let decoratedText = new DecoratedText(
-        `<font color="${statusColor[jobStatus as StatusColorKey]}">${
-          statusMessage[jobStatus as StatusMessageKey]
-        }</font>`,
-        {
-          startIcon: {
-            iconUrl: statusImage[jobStatus as StatusImageKey],
-          },
-        }
-      );
-      sections.addSectionItem(
-        new SectionItem("Status", false, 0, [decoratedText])
-      );
-    }
-
-    if (this.inputJson.body) {
-      let paragraph = new Paragraph(this.inputJson.body);
-      sections.addSectionItem(
-        new SectionItem("Description", false, 0, [paragraph])
-      );
-    }
-
-    sections.addSectionItem(
-      new SectionItem("Creator", true, 1, [
-        new Paragraph(
-          this.inputJson.creator_name || this.githubContext.actor()
-        ),
-      ])
-    );
-
-    let commitUrl: String = "";
-    if (GithubContext.isGithubEnv) {
-      commitUrl = `https://github.com/${this.githubContext.owner()}/${this.githubContext.repo()}/commit/${this.githubContext.sha()}`;
-    } else {
-      commitUrl = "https://xseededucation.com";
-    }
-    sections.addSectionItem(
-      new SectionItem("Commit Id", true, 1, [
-        new Paragraph(
-          `<a style="text-decoration: none; color: red;" href=${commitUrl}>${
-            
-            this.inputJson.commit_id || this.githubContext.sha()
-          }</a>`
-        ),
-      ])
-    );
-
-    let branchUrl = "";
-    if (GithubContext.isGithubEnv) {
-      branchUrl = `https://github.com/${this.githubContext.owner()}/${this.githubContext.repo()}/tree/${this.githubContext.ref()}`;
-    } else {
-      branchUrl = "https://xseededucation.com";
-    }
-    sections.addSectionItem(
-      new SectionItem("Branch", true, 1, [
-        new DecoratedText(
-          `<a style="text-decoration: none; color: red;" href=${branchUrl}>${this.githubContext.ref()}</a>`,
-          {
-            startIcon: {
-              iconUrl: branchIconUrl,
-            },
-          }
-        ),
-      ])
-    );
-    const buttonList = new ButtonList([
-      new Button(
-        "Open Repo",
-        {
-          red: 0.5,
-          green: 0,
-          blue: 1,
-          alpha: 1,
-        },
-        {
-          openLink: {
-            url: `https://github.com/${this.githubContext.owner()}/${this.githubContext.repo()}`,
-          },
-        }
-      ),
-    ]);
-
-    if (this.inputJson.asset_url) {
-      buttonList.addButton(
-        new Button(
-          "Download APK",
-          {
-            red: 0,
-            green: 0.5,
-            blue: 0,
-            alpha: 1,
-          },
-          {
-            openLink: {
-              url: this.inputJson.asset_url,
-            },
-          }
-        )
-      );
-    }
-    sections.addSectionItem(new SectionItem("", false, 0, [buttonList]));
-
-    return sections.json();
+    return new BodyConstructor(this.inputJson, this.githubContext).process();
   }
 
   header(): Record<string, any> {
